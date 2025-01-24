27eba3ecb5eb20e7a0f1624bead56f5faf3611bb
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-01-24 19:08:18 -0300

    feat: add initial product dialog

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,9 @@
-import { PlusIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { DataTable } from "../_components/ui/data-table";
 import { productsTableColumns } from "./_components/table-columns";
 import { getProducts } from "../_data-access/product/get-products";
+import AddProductButton from "./_components/add-product-button";
+
+export const revalidate = 10;
 
 const Products = async () => {
   // chamar o banco de dados
@@ -14,20 +15,15 @@ const Products = async () => {
       <div className="flex w-full items-center justify-between">
         <div className="space-y-1">
           <span className="text-xs font-semibold text-slate-500">
+            {/* <span>Random Number API: {randomNumber}</span> */}
             Gestão de produtos
           </span>
           <p className="text-xl font-semibold">Produtos</p>
         </div>
-        <Button className="add-product gap-2">
-          <PlusIcon size={20} />
-          Novo Produto
-        </Button>
+        <AddProductButton />
       </div>
       {/* Direita */}
-      <DataTable
-        columns={productsTableColumns}
-        data={JSON.parse(JSON.stringify(products))}
-      />
+      <DataTable columns={productsTableColumns} data={products} />
     </div>
   );
 };
